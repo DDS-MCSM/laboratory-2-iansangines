fac05f10ee4678b45c65fd50913bebe08aefec8b
Ian <iansangines@gmail.com>
2018-12-31 15:13:34 +0100

    Not working?????

--- cpe.R
+++ cpe.R
@@ -6,8 +6,8 @@
 #                                                                              #
 #******************************************************************************#
 
-# install.packages("xml2")
-# library(xml2)
+#install.packages("xml2")
+library(xml2)
 
 # compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
 # cpes_filename <- "cpes.zip"
@@ -15,10 +15,26 @@
 # unzip(zipfile = cpes_filename)
 cpe.file <- "./official-cpe-dictionary_v2.3.xml"
 
-GetCPEItems <- function(cpe.raw) {
-  cpe <- NewCPEItem()
-  cpe.raw <- xml2::xml_find_all(cpe.raw)
+DownloadCPEFile <- function()
+{
+  compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
+  cpes_filename <- "cpes.zip"
+  download.file(compressed_cpes_url, cpes_filename)
+  unzip(zipfile = cpes_filename)
+}
 
+
+GetCPEItems <- function(cpes) {
+  #cpe <- NewCPEItem()
+  #cpe.raw <- xml2::read_xml(CPE_XMLfile)
+  cpe.file <- "./official-cpe-dictionary_v2.3.xml"
+  cpes <- xml2::read_xml(cpe.file)
+  cpe.names <- xml2::xml_attr(xml2::xml_find_all(cpes, "//cpe-item"), "name")
+  cpe.titles <- xml2::xml_text(xml2::xml_find_all(cpes,"//cpe-item/title"))
+  cpe.cpe23names <- xml2::xml_attr(xml2::xml_find_all(cpes, "//cpe-item/cpe-23:cpe23-item"), "name")
+  cpeItems <- data.frame(CPE_Name = cpe.names, CPE_Title = cpe.titles, CPE23_name = cpe.cpe23names)
+
+  return(cpeItems)
   # transform the list to data frame
 
   # return data frame
@@ -33,8 +49,11 @@ CleanCPEs <- function(cpes){
 
 ParseCPEData <- function(cpe.file) {
 
+  cpe.file <- "./official-cpe-dictionary_v2.3.xml"
   # load cpes as xml file
   cpes <- xml2::read_xml(x = cpe.file)
+  cpes
+  #valid <- xml2::xml_validate(cpes)
 
   # get CPEs
   cpes <- GetCPEItems(cpes)
