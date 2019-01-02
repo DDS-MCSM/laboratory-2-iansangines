c347dec23bb8283bd4b12a30bdc19d7f3ad0fc49
Ian <iansangines@gmail.com>
2019-01-02 11:45:12 +0100

    Implement GetCPEItems ( and working)

--- cpe.R
+++ cpe.R
@@ -6,14 +6,8 @@
 #                                                                              #
 #******************************************************************************#
 
-#install.packages("xml2")
 library(xml2)
 
-# compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
-# cpes_filename <- "cpes.zip"
-# download.file(compressed_cpes_url, cpes_filename)
-# unzip(zipfile = cpes_filename)
-cpe.file <- "./official-cpe-dictionary_v2.3.xml"
 
 DownloadCPEFile <- function()
 {
@@ -21,23 +15,22 @@ DownloadCPEFile <- function()
   cpes_filename <- "cpes.zip"
   download.file(compressed_cpes_url, cpes_filename)
   unzip(zipfile = cpes_filename)
+  return("./official-cpe-dictionary_v2.3.xml")
 }
 
 
 GetCPEItems <- function(cpes) {
-  #cpe <- NewCPEItem()
-  #cpe.raw <- xml2::read_xml(CPE_XMLfile)
-  cpe.file <- "./official-cpe-dictionary_v2.3.xml"
-  cpes <- xml2::read_xml(cpe.file)
-  cpe.names <- xml2::xml_attr(xml2::xml_find_all(cpes, "//cpe-item"), "name")
-  cpe.titles <- xml2::xml_text(xml2::xml_find_all(cpes,"//cpe-item/title"))
-  cpe.cpe23names <- xml2::xml_attr(xml2::xml_find_all(cpes, "//cpe-item/cpe-23:cpe23-item"), "name")
+  cpe.raw <- xml_find_all(cpes, "//d1:cpe-item")
+  cpe.titles <- xml_text(xml_find_all(cpe.raw, "./d1:title[@xml:lang='en-US']/text()"))
+  cpe.names <- xml_text(xml_find_all(cpe.raw, "./@name"))
+  cpe.cpe23names <- xml_text(xml_find_all(cpe.raw, "./cpe-23:cpe23-item/@name"))
+
+  # transform the list to data frame
   cpeItems <- data.frame(CPE_Name = cpe.names, CPE_Title = cpe.titles, CPE23_name = cpe.cpe23names)
 
-  return(cpeItems)
-  # transform the list to data frame
-
   # return data frame
+  return(cpeItems)
+
 }
 
 CleanCPEs <- function(cpes){
@@ -49,18 +42,22 @@ CleanCPEs <- function(cpes){
 
 ParseCPEData <- function(cpe.file) {
 
-  cpe.file <- "./official-cpe-dictionary_v2.3.xml"
   # load cpes as xml file
-  cpes <- xml2::read_xml(x = cpe.file)
-  cpes
+  cpes_fromfile <- xml2::read_xml(x = cpe.file)
   #valid <- xml2::xml_validate(cpes)
 
   # get CPEs
-  cpes <- GetCPEItems(cpes)
+  cpes <- GetCPEItems(cpes_fromfile)
+
 
   # transform, clean, arrange parsed cpes as data frame
-  df <- CleanCPEs(cpes)
+  #df <- CleanCPEs(cpes)
 
   # return data frame
+  df <- cpes
   return(df)
 }
+
+### Main ---------------------------------------------------------------------------------------
+file <- DownloadCPEFile()
+ParseCPEData(file)
